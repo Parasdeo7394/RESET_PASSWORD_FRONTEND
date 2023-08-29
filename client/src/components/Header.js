00aae91babf435213ce569ef0019f70d4f8e5a50
Parasdeo7394 <paras7394@gmail.com>
2023-08-29 12:07:35 +0530

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,125 @@
+import React, { useContext } from "react";
+import Avatar from "@mui/material/Avatar";
+import { LoginContext } from "./ContextProvider/Context";
+// import Menu from '@mui/material/Menu';
+import { Menu } from "@mui/material";
+// import MenuItem from '@mui/material/MenuItem';
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const { logindata, setLoginData } = useContext(LoginContext);
+
+  const history = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logoutuser = async () => {
+    let token = localStorage.getItem("usersdatatoken");
+
+    const res = await fetch("/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+        Accept: "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (data.status === 201) {
+      console.log("user logout");
+      let token = localStorage.removeItem("usersdatatoken");
+      setLoginData(false);
+      history("/");
+    } else {
+      console.log("error");
+    }
+  };
+
+  const goDash = () => {
+    history("/dash");
+  };
+
+  const goError = () => {
+    history("*");
+  };
+
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>Hp Cloud</h1>
+          <div className="avtar">
+            {logindata.validUserOne ? (
+              <Avatar
+                style={{
+                  background: "salmon",
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                }}
+                onClick={handleClick}
+              >
+                {logindata.validUserOne.fname[0].toUpperCase()}
+              </Avatar>
+            ) : (
+              <Avatar style={{ background: "blue" }} onClick={handleClick} />
+            )}
+          </div>
+
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            {logindata.validUserOne
+              ? [
+                  <MenuItem
+                    onClick={() => {
+                      goDash()
+                      handleClose()
+                    }}
+                  >
+                    Profile
+                  </MenuItem>,
+                  <MenuItem
+                    onClick={() => {
+                      logoutuser()
+                      handleClose()
+                    }}
+                  >
+                    Logout
+                  </MenuItem>,
+                ]
+              : [
+                  <MenuItem
+                    onClick={() => {
+                      goError()
+                      handleClose()
+                    }}
+                  >
+                    Profile
+                  </MenuItem>,
+                ]}
+          </Menu>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
